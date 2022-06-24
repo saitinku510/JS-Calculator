8b2195a8fdad5c18116a8a717d5e16c594e082c5
venkatasai510 <venkatasai@sketchbrahma.com>
2022-06-24 10:17:51 +0530

    Data Capture in an array

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ let display = document.getElementById("display");
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 
+let dataCapture = [];
+
+
 buttons.map(button => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
@@ -17,10 +20,17 @@ buttons.map(button => {
             case '=':
                 try{
                     let data = document.getElementById('data');
-                    data.innerHTML = display.innerText;
+                    data = display.innerText;
                     display.innerText = eval(display.innerText);
                     let result = document.getElementById('result');
-                    result.innerHTML = display.innerText;
+                    result = display.innerText;
+                    let obj = {
+                        data : data,
+                        result : result,
+                    }
+                    dataCapture.push(obj);
+                    console.log(dataCapture);	
+                    genUI();
                 }catch {
                     display.innerText = 'Error!';
                 }
@@ -31,3 +41,19 @@ buttons.map(button => {
     });
 });
 
+function genUI(){
+    document.getElementById("tablebody").innerHTML="";
+    dataCapture.forEach(function(s,i){
+    var nr=document.createElement("tr")
+    nr.innerHTML=i+1
+                                            
+    for(var k in s){
+        var nd=document.createElement("td")
+        nd.innerHTML=s[k]
+        nr.appendChild(nd);															
+        }
+                    
+    document.getElementById("tablebody").appendChild(nr)											
+    });
+    
+}
